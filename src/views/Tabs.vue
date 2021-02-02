6708589e2948fb82b278c08a31b95070f3927a0b
Alvaro Saburido <alvaro.saburido@gmail.com>
2021-02-02 17:40:43 +0100

    Tabs dynamically

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -2,19 +2,14 @@
   <ion-page>
     <ion-tabs>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
-        </ion-tab-button>
-          
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
-        </ion-tab-button>
-        
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+        <ion-tab-button
+          :tab="tab.slug"
+          :href="`/tabs/${tab.slug}`"
+          v-for="tab in tabs"
+          :key="tab.slug"
+        >
+          <ion-icon :icon="tab.icon" />
+          <ion-label>{{ tab.label }}</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -22,18 +17,41 @@
 </template>
 
 <script lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import {
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonLabel,
+  IonIcon,
+  IonPage,
+} from "@ionic/vue";
+import { ellipse, square, triangle } from "ionicons/icons";
+import { ref } from "vue";
 
 export default {
-  name: 'Tabs',
+  name: "Tabs",
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
   setup() {
+    const tabs = ref([
+      {
+        icon: triangle,
+        slug: "tab1",
+        label: "Tab 1",
+      },
+      {
+        icon: ellipse,
+        slug: "tab2",
+        label: "Tab 2",
+      },
+      {
+        icon: square,
+        slug: "tab3",
+        label: "Tab 3",
+      },
+    ]);
     return {
-      ellipse, 
-      square, 
-      triangle,
-    }
-  }
-}
+      tabs,
+    };
+  },
+};
 </script>
